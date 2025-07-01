c2874fee3d8f475351ec796b799585a2bdb91e76
Piotr Szumera <piotr.szumera@esky.com>
2025-07-01 10:06:02 +0200

    app controller test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { INITIALIZE_CHARACTERS_COMMAND } from './characters/application/commands/initialize-character.command';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,7 +9,11 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService,
+        {
+          provide: INITIALIZE_CHARACTERS_COMMAND,
+          useValue: { execute: jest.fn() }
+        }],
     }).compile();
 
     appController = app.get<AppController>(AppController);
